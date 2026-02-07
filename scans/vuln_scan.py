1cd59b9495083a36027c7dfc43e1df4763fb8c55
nandha-k-pixel <nandhakumar9385@gmail.com>
2026-02-07 15:59:38 +0000

    v1.1.1 Fix missing subprocess import in nuclei scanner

--- scans/vuln_scan.py
+++ scans/vuln_scan.py
@@ -1,19 +1,22 @@
 import subprocess
 
-def run_nuclei(live_hosts_file):
+def run_nuclei(live_hosts):
     print("[+] Running Nuclei vulnerability scan...")
 
-    output_file = "scans/nuclei_results.txt"
-
-    command = [
-        "nuclei",
-        "-l", live_hosts_file,
-        "-severity", "medium,high,critical",
-        "-o", output_file
+    template_paths = [
+        "~/nuclei-templates/http/cves",
+        "~/nuclei-templates/http/exposures",
+        "~/nuclei-templates/cloud",
+        "~/nuclei-templates/dns"
     ]
 
-    subprocess.run(command)
-
-    print(f"[+] Nuclei scan completed")
-    print(f"[+] Results saved to {output_file}")
+    for path in template_paths:
+        name = path.split("/")[-1]
+        cmd = (
+            f"nuclei -l {live_hosts} "
+            f"-t {path} "
+            f"-severity medium,high,critical "
+            f"-o scans/{name}_results.txt"
+        )
+        subprocess.run(cmd, shell=True)
 
